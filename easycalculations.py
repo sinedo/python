43f3b79dfcbbe9c53ba21aa5b1d9f1d2d25ec4e7
sinedo <donimobile1@gmail.com>
2020-03-07 17:18:31 +0100

    progress

--- easycalculations.py
+++ easycalculations.py
@@ -1,4 +1,46 @@
-a=5
-b=3
-d=a+b
-print(d)
+count=0
+number1=""
+operator=""
+number2=""
+result=""
+
+print("easy calculation\n================")
+
+
+while 1:
+    while 1:
+        number1 = input("first number: ")
+        try:
+            number1=inputnumber(number1)
+            break
+        except:
+            error(1)
+        
+'''
+    number1=inputnumber(number1)
+    operator=print("operator: ")
+    inputoperator(operator)
+    number2=input("second number: ")
+    inputnumber(number2)
+    calculate(number1, number2, operator)
+'''
+
+
+
+
+
+
+
+def error(nr):
+    if nr == 1:
+        print("enter a number, not a text")
+        
+
+def inputnumber(string):
+    string=string.replace(",",".")
+    return float(string)
+
+
+#def inputoperator(string):
+
+#def calculate(n1,n2,o):
