c9f73c1150fbd29f6e72b9d710b1453bb3731d09
sinedo <donimobile1@gmail.com>
2020-03-07 20:13:17 +0100

    read value for first number

--- easycalculations.py
+++ easycalculations.py
@@ -1,46 +1,14 @@
-count=0
-number1=""
-operator=""
-number2=""
-result=""
-
 print("easy calculation\n================")
 
-
 while 1:
+
     while 1:
         number1 = input("first number: ")
         try:
-            number1=inputnumber(number1)
+            number1=float(number1.replace(",","."))
             break
         except:
-            error(1)
+            print("enter a number, not a text")
+    while 1:
+        allowedoperators = '+-*/%'
         
-'''
-    number1=inputnumber(number1)
-    operator=print("operator: ")
-    inputoperator(operator)
-    number2=input("second number: ")
-    inputnumber(number2)
-    calculate(number1, number2, operator)
-'''
-
-
-
-
-
-
-
-def error(nr):
-    if nr == 1:
-        print("enter a number, not a text")
-        
-
-def inputnumber(string):
-    string=string.replace(",",".")
-    return float(string)
-
-
-#def inputoperator(string):
-
-#def calculate(n1,n2,o):
