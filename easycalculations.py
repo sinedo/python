b40bb5b6bbf82b3260222d42bcfdf309e7286e9b
sinedo <donimobile1@gmail.com>
2020-03-08 13:24:07 +0100

    basic dumb calculator without gui is finished

--- easycalculations.py
+++ easycalculations.py
@@ -9,7 +9,29 @@ while 1:
             break
         except:
             print("enter a number, not a text")
+    
+    #read operator until allowed one
+    allowedoperators = '+-*/%'
+    operator="aa"
+    while len(operator) != 1 or operator not in allowedoperators:
+        operator=input("operator: ")
+
     while 1:
-        #read operator until allowed one
-        allowedoperators = '+-*/'
-        
+        number2 = input("second number: ")
+        try:
+            number2=float(number2.replace(",","."))
+            break
+        except:
+            print("enter a number, not a text")
+
+    if operator == "+":
+        result=number1+number2
+    if operator == "-":
+        result=number1-number2
+    if operator == "*":
+        result=number1*number2
+    if operator == "/":
+        result=number1/number2
+
+    string=str(number1)+operator+str(number2)+"= "+str(result)
+    print(string)
